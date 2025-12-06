e3ff7235ceb75b6adeb978394552b87eebc1a8ff
Damian Cutajar <damian.cutajar@gmail.com>
2025-12-06 03:33:11 +0100

    Removed MetaTailor

--- OperationDuck/Plugins/MetaTailorBridge/Source/MetaTailorBridgeRuntime/Private/MetaTailorBridgeRuntime.cpp
+++
@@ -1,4 +0,0 @@
-
-#include "MetaTailorBridgeRuntime.h"
-
-IMPLEMENT_MODULE(FMetaTailorBridgeRuntimeModule, MetaTailorBridgeRuntime)
--- OperationDuck/Plugins/MetaTailorBridge/Source/MetaTailorBridgeRuntime/Public/MetaTailorBridgeRuntime.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "Modules/ModuleManager.h"
-
-class METATAILORBRIDGERUNTIME_API FMetaTailorBridgeRuntimeModule : public IModuleInterface
-{
-	
-};
